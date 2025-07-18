35adb545871054e75b05855d9494a31039caa8bb
not-arslan <arslan.2013.01.02@gmail.com>
2025-07-18 21:38:34 +0530

    Update app.py

--- app.py
+++ app.py
@@ -2,9 +2,11 @@ import streamlit as st
 from modules.login import login_ui
 from modules.dashboard import show_dashboard
 
+# Session-based login system
 if "auth" not in st.session_state:
     st.session_state.auth = False
 
+# If authenticated, show dashboard
 if st.session_state.auth:
     show_dashboard()
 else:

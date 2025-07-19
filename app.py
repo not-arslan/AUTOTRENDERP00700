f9b0dac4c0623899b9766d2097827462719343d9
not-arslan <arslan.2013.01.02@gmail.com>
2025-07-19 18:58:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,18 +1,17 @@
-# app.py
 import streamlit as st
 from modules.login import login_user
 from modules.dashboard import show_dashboard
 
 def main():
-    st.set_page_config(page_title="FS Traders", layout="wide")
+    st.set_page_config(page_title="FS Traders Official", layout="wide")
 
-    if "logged_in" not in st.session_state:
-        st.session_state.logged_in = False
+    if "authenticated" not in st.session_state:
+        st.session_state.authenticated = False
 
-    if st.session_state.logged_in:
-        show_dashboard()  # 👈 Your actual dashboard
-    else:
+    if not st.session_state.authenticated:
         login_user()
+    else:
+        show_dashboard()
 
 if __name__ == "__main__":
     main()

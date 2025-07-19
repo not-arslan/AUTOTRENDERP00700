fcd57cfd80c66c06add0b9241fdc4a5b1909f09e
not-arslan <arslan.2013.01.02@gmail.com>
2025-07-19 18:51:03 +0530

    Update login.py

--- modules/login.py
+++ modules/login.py
@@ -1,13 +1,14 @@
 # modules/login.py
-
 import streamlit as st
 
 def login_user():
     st.title("Login Page")
     username = st.text_input("Username")
     password = st.text_input("Password", type="password")
+
     if st.button("Login"):
         if username == "admin" and password == "admin123":
             st.success("Login Successful ✅")
+            st.session_state.logged_in = True  # 👈 set login flag
         else:
             st.error("Invalid credentials ❌")
